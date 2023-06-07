7f8fee709a4ba29eb483e33664fb86949134106c
Ishant Chauhan <ishantchauhan710@gmail.com>
2023-06-07 23:21:47 +0530

    Electron file picker implemented

--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
-const { app, BrowserWindow } = require("electron");
+const { app, ipcMain, dialog, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
@@ -25,4 +28,15 @@ app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+ipcMain.on("openDirectoryPicker", (event, args) => {
+  dialog.showOpenDialog({ properties: ["openFile"] }).then(function (response) {
+    if (!response.canceled) {
+      // handle fully qualified file name
+      console.log(response.filePaths[0]);
+    } else {
+      console.log("no file selected");
+    }
+  });
 });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { FaFolderPlus } from "react-icons/fa";
+const { ipcRenderer } = window.require("electron");
+
 function App() {
-  return <div className="bg-yellow-500 text-red-500">Hello World</div>;
+  const openDirectoryPicker = () => {
+    ipcRenderer.send("openDirectoryPicker");
+  };
+
+  return (
+    <div className="bg-neutral-900 text-white h-screen flex items-center justify-center">
+      <div className="w-1/2 h-1/2 flex items-center justify-center">
+        <div
+          onClick={openDirectoryPicker}
+          className="w-1/2 h-[200px] text-7xl bg-neutral-950 rounded-md flex items-center justify-center flex-col cursor-pointer hover:bg-neutral-950/70"
+        >
+          <FaFolderPlus />
+          <div className="mt-2 text-lg">Select a folder</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
